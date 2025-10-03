22fa29b2f51847826ce33a20388dfb3fbe42ca59
Pratima-Sahuji <167970416+Pratima-Sahuji@users.noreply.github.com>
2025-10-03 16:37:50 +0530

    Update TeacherAttendance.jsx

--- frontend/src/TeacherAttendance.jsx
+++ frontend/src/TeacherAttendance.jsx
@@ -11,7 +11,7 @@ function TeacherAttendance() {
 
   const fetchStudents = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/students", {
+      const res = await axios.get("https://mini-school-portal-backend.onrender.com/api/students", {
         headers: { Authorization: `Bearer ${token}` },
       });
       setStudents(res.data);
@@ -36,7 +36,7 @@ function TeacherAttendance() {
         if (!status) continue; 
 
         await axios.post(
-          "http://localhost:5000/api/attendance/",
+          "https://mini-school-portal-backend.onrender.com/api/attendance/",
           {
             student_id: student.id,
             date: new Date().toISOString().split("T")[0], 
