2a9037bbb0c27fb69ad5eba316c965515ce2a3a5
Pratima-Sahuji <167970416+Pratima-Sahuji@users.noreply.github.com>
2025-10-06 02:29:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mini-school-portal-backend.onrender.com',
         changeOrigin: true,
         secure: true,
       },
